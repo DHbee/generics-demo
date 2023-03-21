9512e486c51ecbb1eea19e5ccc01ab06e22e29cb
psaradhi <parthasaradhi.malayathi@dharbor.com>
2023-03-21 07:11:48 +0530

    modified generic class

--- src/main/java/com/dharbor/generics/demo/classlevel/GenericClassDemo1.java
+++ src/main/java/com/dharbor/generics/demo/classlevel/GenericClassDemo1.java
@@ -13,10 +13,16 @@ public class GenericClassDemo1 <T> {
         System.out.println(value);
     }
     public static void main(String[] args) {
-        GenericClassDemo1 genericMethodDemo1 = new GenericClassDemo1();
+        GenericClassDemo1<Integer> genericMethodDemo1 = new GenericClassDemo1<>();
         genericMethodDemo1.demo1(10);
-        genericMethodDemo1.demo1("10");
-        genericMethodDemo1.demo1(new ArrayList<>());
-        genericMethodDemo1.demo1(new HashMap<>());
+
+        GenericClassDemo1<String> genericMethodDemo2 = new GenericClassDemo1<>();
+        genericMethodDemo2.demo1("10");
+
+        GenericClassDemo1<ArrayList> genericMethodDemo3 = new GenericClassDemo1<>();
+        genericMethodDemo3.demo1(new ArrayList<>());
+
+        GenericClassDemo1<HashMap> genericMethodDemo4 = new GenericClassDemo1<>();
+        genericMethodDemo4.demo1(new HashMap<>());
     }
 }
